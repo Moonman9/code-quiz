2b3d6c8bcdafd11967fc38e39d10f11c349721d0
Moonman9 <zzepold97@gmail.com>
2022-02-27 21:35:45 -0700

    fix bug

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,14 +1,20 @@
-function displayScores(){
+// function to post highscores 
+function displayScores() {
     var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
     highscores.sort(function(a,b)
     {
         return b.score - a.score;
     });
-    highscores.highscores.forEach(score => {
+    highscores.forEach(function(score) {
         var liTag = document.createElement('li');
         liTag.textContent = score.initials + " - " + score.score;
         var olEl = document.getElementById('highscore-list');
         olEl.appendChild(liTag);
     });
 }
-function
+function deleteScores() {
+    window.localStorage.removeItem('highscores');
+    window.location.reload();
+}
+document.getElementById('clear-btn').onclick = deleteScores;
+displayScores();
